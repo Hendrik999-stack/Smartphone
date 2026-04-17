999153a707d7a5c5de477729155067dae9cf0a57
Hendrik Albert <hendrikalberthus7@gmail.com>
2026-04-17 10:28:20 +0700

    Interface Mengambil Gambar menggunakan Iphone dan Samsung

--- Smartphone/Program.cs
+++ Smartphone/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ISmartphone hpBudi = new Iphone();
+
+Fotografer fotografer = new Fotografer();
+fotografer.Kerja(hpBudi);
+
+hpBudi = new Samsung();
+fotografer.Kerja(hpBudi);
+
+interface ISmartphone
+{
+    public void AmbilFoto();
+}
+
+class Iphone : ISmartphone
+{
+    public void AmbilFoto()
+    {
+        Console.WriteLine("Mengaktifkan sensor kamera premium ... Cekrek");
+    }
+   
+}
+
+class Samsung : ISmartphone
+{
+    public void AmbilFoto()
+    {
+        Console.WriteLine("Mengaktifkan fitur beauty ... Cekrek");
+    }
+}
+
+class Fotografer
+{
+    public void Kerja(ISmartphone Hp)
+    {
+        Hp.AmbilFoto();
+    }
+}
